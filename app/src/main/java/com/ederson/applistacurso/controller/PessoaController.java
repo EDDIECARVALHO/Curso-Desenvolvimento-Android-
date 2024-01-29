0d56431552a58df64e15754cea61632199a40c5d
Eddi <eddyncarvalho@gmail.com>
2024-01-29 18:31:27 -0300

    Refatorada a Classe PessoaController

--- app/src/main/java/com/ederson/applistacurso/controller/PessoaController.java
+++ app/src/main/java/com/ederson/applistacurso/controller/PessoaController.java
@@ -1,12 +1,24 @@
 package com.ederson.applistacurso.controller;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.ederson.applistacurso.model.Pessoa;
+import com.ederson.applistacurso.view.MainActivity;
 
 public class PessoaController {
+    SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
+    public static final String NOME_PREFERENCES = "pref_listavip";
+
+
+    public PessoaController(MainActivity mainActivity) {
+        preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+        SharedPreferences.Editor listaVip;
+        listaVip = preferences.edit();
+    }
 
     @NonNull
     @Override
@@ -19,9 +31,30 @@ public class PessoaController {
 
 
     }
-    public void salvar(Pessoa pessoa){
-         Log.d("MVC_Controller", "Salvo: "+pessoa.toString());
+
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC_Controller", "Salvo: " + pessoa.toString());
+
+        listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaVip.putString("sobreNome", pessoa.getSobreNome());
+        listaVip.putString("nomeCurso", pessoa.getCursoDesejado());
+        listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaVip.apply();
 
     }
 
+    public Pessoa buscar(Pessoa pessoa) {
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", ""));
+        pessoa.setSobreNome(preferences.getString("sobreNome", ""));
+        pessoa.setCursoDesejado(preferences.getString("nomeCurso", ""));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", ""));
+
+        return pessoa;
+    }
+
+    public void limpar() {
+        listaVip.clear();
+        listaVip.apply();
+
+    }
 }
--- app/src/main/java/com/ederson/applistacurso/view/MainActivity.java
+++ app/src/main/java/com/ederson/applistacurso/view/MainActivity.java
@@ -16,9 +16,7 @@ import com.ederson.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
-    SharedPreferences preferences;
-    SharedPreferences.Editor listaVip;
-    public static final String NOME_PREFERENCES = "pref_listavip";
+
 
 
     Pessoa pessoa;
@@ -42,20 +40,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        preferences = getSharedPreferences(NOME_PREFERENCES, 0);
-         listaVip = preferences.edit();
 
-        PessoaController controler = new PessoaController();
-
-
-        pessoa = new Pessoa();
-
-
+        PessoaController controler = new PessoaController(MainActivity.this);
+        //controller = new PessoaController(mainActive:MainActivity.this)
+        //getMediaController().toString()
         String defValue;
-        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", ""));
-        pessoa.setSobreNome(preferences.getString("sobreNome", ""));
-        pessoa.setCursoDesejado(preferences.getString("nomeCurso", ""));
-        pessoa.setTelefoneContato(preferences.getString("telefoneContato", ""));
+       // pessoa = new Pessoa();
 
         editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
         editSobreNomeAluno = findViewById(R.id.editSobreNomeAluno);
@@ -79,8 +69,8 @@ public class MainActivity extends AppCompatActivity {
                 editTelefoneContato.setText("");
                 editNomeCurso.setText("");
 
-                listaVip.clear();
-                listaVip.apply();
+                PessoaController controller = null;
+                controller.limpar();
             }
         });
 
@@ -103,11 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Toast.makeText(MainActivity.this, "Salvo " + pessoa.toString(), Toast.LENGTH_LONG).show();
 
-                listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
-                listaVip.putString("sobreNome", pessoa.getSobreNome());
-                listaVip.putString("nomeCurso", pessoa.getCursoDesejado());
-                listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
-                listaVip.apply();
+
 
                 PessoaController controller = null;
                 controller.salvar(pessoa);
